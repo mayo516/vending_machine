145620f6674709487148151812e7778235abadd4
eunha <dmsgk2323@naver.com>
2022-10-14 21:38:15 +0900

    장바구니에 추가하는 기능 생성

--- js/app.js
+++ js/app.js
@@ -5,14 +5,37 @@ const coolCola = document.querySelector('#coolCola');
 const greenCola = document.querySelector('#greenCola');
 const orangeCola = document.querySelector('#orangeCola');
 
-const productList = [originalCola, violetCol, yellowCola, coolCola, greenCola, orangeCola];
+const productList = [originalCola, violetCola, yellowCola, coolCola, greenCola, orangeCola];
 
 
 productList.forEach((name) => {
     name.addEventListener('click', function () {
-        name.classList.toggle('product-wrapper-click')
+
+        name.classList.toggle('product-wrapper-click');
+        const newItem = document.createElement('li');
+        const shoppingList = document.querySelector('#shoppingList');
+
+        if (name.classList.contains('product-wrapper-click') === true) {
+
+            shoppingList.appendChild(newItem);
+            newItem.classList += `shopping-item`;
+
+            newItem.id = `${name.id}2`;
+            newItem.innerHTML = `<img src="img/콜라.svg" alt="오리지널콜라">
+            <div div class="shopping-item-name" > ${name.id}</div >
+                <div class="shopping-num">1</div>`
+
+        } else if (name.classList.contains('product-wrapper-click') === false) {
+            removeItem = document.querySelector(`#${name.id}2`);
+            shoppingList.removeChild(removeItem);
+
+        }
+        // console.log(name.childNodes[3].childNodes);
     })
 });
+
+
+
 
 
 
