e63f45470b1b3c53585b5e387d92314169fe6507
eunha <dmsgk2323@naver.com>
2022-10-15 18:04:23 +0900

    입금 버튼 기능 구현 완료

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// 기능1 : 상품 추가하면 장바구니에 담기는 기능
+
 const originalCola = document.querySelector('#originalCola');
 const violetCol = document.querySelector('#violetCola');
 const yellowCola = document.querySelector('#yellowCola');
@@ -21,7 +23,7 @@ productList.forEach((name) => {
             newItem.classList += `shopping-item`;
 
             newItem.id = `${name.id}2`;
-            newItem.innerHTML = `<img src="img/콜라.svg" alt="오리지널콜라">
+            newItem.innerHTML = `<img src="img/${name.id}.svg" alt="오리지널콜라">
             <div div class="shopping-item-name" > ${name.id}</div >
                 <div class="shopping-num">1</div>`
 
@@ -33,6 +35,44 @@ productList.forEach((name) => {
         // console.log(name.childNodes[3].childNodes);
     })
 });
+
+
+
+// 입금 버튼 누르면 일어나는 일  
+const moneyInput = document.querySelector('#moneyInput');
+const moneyButton = document.querySelector('#moneyButton');
+const balanceMoney = document.querySelector('#balanceMoney');
+const moneyNum = document.querySelector('#moneyNum');
+console.log(parseInt(moneyNum.innerText.slice(0, 6)));
+
+
+moneyButton.addEventListener('click', function () {
+    // 입금액이 없는 경우 (alert나 0을 더하는 것)
+    if (moneyInput.value === '') {
+        alert('입금액을 임력하세요')
+    }
+    //입금액이 소지금보다 큰 경우 ( alert )
+    else if (parseInt(moneyNum.innerText.slice(0, 6).replace(/,/g, '')) - parseInt(moneyInput.value) < 0) {
+        alert('잔액이 부족합니다')
+        moneyInput.value = '입금액 입력';
+    }
+    else { // 1. 잔액에 입금액 만큼이 추가 되어야 한다. 
+        let change = balanceMoney.innerText;
+
+        change = parseInt(change) + parseInt(moneyInput.value);
+        balanceMoney.innerText = `${change}원`
+        // console.log(parseInt(balanceMoney));
+
+        // 2. 소지금에서 입금액 만큼이 빠져야 한다. 
+        // 소지금 - moneyInput.value
+        moneyNum.innerText = `${parseInt(moneyNum.innerText.slice(0, 6).replace(/,/g, '')) - parseInt(moneyInput.value)}원`;
+
+
+        // 3. 입금액이 초기화 되어야한다. 
+        moneyInput.value = '입금액 입력';
+    }
+})
+
 
 
 
